44557534a1e0ed7bb959c07ea8a3564aff75f9c5
mell <mecaicedo@est.ecotec.edu.ec>
2024-07-11 10:59:36 -0500

    cambios

---
+++ GestionApiTareas/Entities/TaskEstudiantes.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+
+namespace GestionApiTareas.Entities
+{
+    public class TaskEstudiantes: CrudEntities
+    {
+        [Key]
+        public long idTask { get; set; }
+
+        [MaxLength(500)]
+        public string idEstudiante { get; set; }
+
+
+        [MaxLength(500)]
+        public  string nombreEstuiante { get; set; }
+
+        public long nota { get; set; }
+
+        public bool estadoNota { get; set; }
+
+
+        [MaxLength(500)]
+        public string descripcion { get; set; }
+
+
+    }
+}
